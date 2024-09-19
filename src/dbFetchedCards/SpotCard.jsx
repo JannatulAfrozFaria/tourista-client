577fa3bd87047e8dff920c275afdb7858ee92d5f
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-19 17:39:00 +0600

    all spots style updated

--- src/dbFetchedCards/SpotCard.jsx
+++ src/dbFetchedCards/SpotCard.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const SpotCard = ({sp}) => {
     const { _id,photo, spot, description,country,location,cost,seasonality,time,visitors,username,email} = sp;
     return (
-        <div className="card-side bg-base-100 shadow-xl">
+        <div className="card-side bg-base-100 shadow-xl shadow-green-100">
             <figure><img className='h-[230px]' src={photo} alt="Album"/></figure>
-            <div className="card-body">
-                <h2 className="card-title">Tourist Spot: {spot} </h2>
+            <div className="grid grid-cols-1 p-6 items-end">
+                <h2 className="card-title">{spot} </h2>
                 <div className="text-left">
                     <p className='theme-light font-medium' > <span className='font-semibold text-gray-500'>Country :</span> {country} </p>
                     <p className='theme-light font-medium' > <span className='font-semibold text-gray-500'>Location :</span> {location} </p>
@@ -16,10 +16,8 @@ const SpotCard = ({sp}) => {
                     <p className='theme-light font-medium' > <span className='font-semibold text-gray-500'>Travel Time :</span> {time} </p>
                     <p className='theme-light font-medium' > <span className='font-semibold text-gray-500'>Seasonality :</span> {seasonality} </p>
                 </div>
-                
-
                 {/* BUTTON-------- */}
-                <div className="card-actions justify-start">
+                <div className="mt-2">
                     <Link to={`/spot/${_id}`}  className='btn basic-btn'>View Details</Link>
                     {/* <Link className='btn basic-btn'>
                         View Details
